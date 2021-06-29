746745ab334b1375e1d72fe16aa9deaf468a3070
Do Your Best <dodbeg@gmail.com>
2021-06-29 23:09:44 +0200

    Network variables declared in modeules

--- aks_k8s_sltn/dev/main.tf
+++ aks_k8s_sltn/dev/main.tf
@@ -1,5 +1,12 @@
 module "dev_k8s" {
-  source       = "../modules/aks"
-  env          = "dev"
-  location     = "northeurope"
+  source           = "../modules/aks"
+  env              = "dev"
+  location         = "northeurope"
+  hubVnetAddress   = ["10.200.0.0/24"]
+  firewallSubnet   = ["10.200.0.0/26"]
+  gatewaysubnet    = ["10.200.0.64/27"]
+  bastionsubnet    = ["10.200.0.96/27"]
+  spokeVnetAddress = ["10.240.0.0/16"] 
+  akssubnet        = ["10.240.0.0/22"]
+  akslbsubnet      = ["10.240.4.0/28"]
   }
